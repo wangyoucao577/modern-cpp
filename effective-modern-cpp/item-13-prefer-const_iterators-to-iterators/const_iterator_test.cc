ca1873b2f9ab1ad0188c3469de43bd50c924aa37
Jay Zhang <wangyoucao577@gmail.com>
2020-02-22 15:57:24 +0800

    feat: add type of iterator

--- effective-modern-cpp/item-13-prefer-const_iterators-to-iterators/const_iterator_test.cc
+++ effective-modern-cpp/item-13-prefer-const_iterators-to-iterators/const_iterator_test.cc
@@ -6,13 +6,18 @@ int main() {
 
     std::vector<int> values{1, 2, 3, 4, 5};
 
-    auto it = std::find(values.cbegin(), values.cend(), 4);
-    std::cout << "iterator type: " << typeid(it).name() 
-        << ", is const: " << std::is_const<decltype(it)>::value << " " << std::is_const<std::vector<int>::const_iterator>::value << std::endl;
+    auto cit = std::find(values.cbegin(), values.cend(), 4);
+    std::cout << "const iterator type: " << typeid(cit).name() 
+        << ", is const: " << std::is_const<decltype(cit)>::value << " " << std::is_const<std::vector<int>::const_iterator>::value << std::endl;
+    values.insert(cit, 100);
 
-    values.insert(it, 100);
+    auto it = std::find(values.begin(), values.end(), 5);
+    std::cout << "      iterator type: " << typeid(it).name() 
+        << ", is const: " << std::is_const<decltype(it)>::value << " " << std::is_const<std::vector<int>::iterator>::value << std::endl;
+    values.insert(it, 200);
 
 
+    std::cout << "values: ";
     for (auto v : values) {
         std::cout << v << " ";
     }
